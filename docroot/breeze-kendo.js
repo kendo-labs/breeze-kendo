5fe54f25e5a641a2a36fc24351c8434d28f17278
Ron DeFreitas <drax3d@gmail.com>
2014-09-23 15:46:34 -0400

    Fix Add Entity for WebApi2 Breeze single controller

--- docroot/breeze-kendo.js
+++ docroot/breeze-kendo.js
@@ -136,7 +136,7 @@
                     break;
                   case "add":
                     ev.items.forEach(function(item){
-                        var entity = manager.createEntity(typeName, item);
+                        var entity = manager.createEntity(typeName || this.query.resourceName, item);
                         manager.addEntity(entity);
                         syncItems(item, entity);
                     });
